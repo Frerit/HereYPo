8c56a104330c8949cb2620fff87d02039e6608b1
Julian Perez <julianperez@strategee.us>
2016-05-24 08:05:46 -0500

    Subisa de Archivos

---
+++ ClaseHeryPol/Herencia/Profesores.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseHeryPol.Herencia
+{
+    public class Profesores
+    {
+        private string _Nombre;
+        private string _Apellido;
+        private int _Materias;
+
+        public string Nombre
+        {
+            get { return _Nombre; }
+            set { _Nombre = value; }
+        }
+        public string Apellido
+        {
+            get { return _Apellido; }
+            set { _Apellido = value; }
+        }
+        public int Materias
+        {
+            get { return _Materias; }
+        }
+    }
+}
